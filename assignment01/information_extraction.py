073859f73cf5f3a2da313abddef151b78a0ce493
shikuan <0915@gmail.com>
2018-02-21 21:41:17 -0500

    assignment#1

--- assignment01/information_extraction.py
+++ assignment01/information_extraction.py
@@ -25,3 +25,4 @@ class Person(object):
         self.dislikes = [] if dislikes is None else dislikes
         self.has = [] if has is None else has
         self.travels = [] if travels is None else travels
+        all()
