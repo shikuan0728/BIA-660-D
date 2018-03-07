7a9f3063abf1db4af6e86180321a3cd6d9266f32
shikuan <0915@gmail.com>
2018-03-07 15:42:16 -0500

    assignment#2

--- assignment02/assignment02.py
+++ assignment02/assignment02.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
-
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
