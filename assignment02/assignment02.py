32dcc376bac2bf69a74cdb0d913d83c34f9f1a7d
shikuan <0915@gmail.com>
2018-03-07 15:41:32 -0500

    assignment#2

--- assignment02/assignment02.py
+++ assignment02/assignment02.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
+
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
 import random
 import time
 import pandas as pd
@@ -24,8 +26,11 @@ season_select = Select(hitting_season_element)
 
 season_select.select_by_value('2015')
 
-
+import bs4
+from selenium.webdriver.common.keys import Keys
 def extract_stats_data(driver):
+    data_div = driver.find_element_by_id('datagrid')
+    data_html = data_div.get_attribute('innerHTML')
     soup = bs4.BeasutifulSoup(data_html, "html5lib")
     soup.thead
     for t in soup.thead.findAll('th'):
@@ -42,3 +47,46 @@ def extract_stats_data(driver):
     pass
 
 team_hr_stats = wait.until(EC.visibility_of_element_located((By.ID, 'datagrid')))
+
+#question 1
+team_click = driver.find_element_by_id('st_parent')
+team_click.click()
+
+seasontype_click = driver.find_element_by_id('st_hitting_game_type')
+seasontype_selcet=Select(seasontype_click)
+seasontype_selcet.select_by_visible_text('Regular Season')
+
+
+data_div = driver.find_element_by_id('datagrid')
+data_html = data_div.get_attribute('innerHTML')
+
+team_2015 = extract_stats_data(data_html)
+team_2015
+
+Team_Name = team_2015.iloc[team_2015['HR'].astype('int64').idxmax()]['Team']
+print('Answer of Q1 "Which team had the most homeruns in the regular season of 2015?" is ',Team_Name)
+#the answer to "Which team had the most homeruns in the regular season of 2015?" is  Toronto Blue Jays
+
+
+#Question 2
+#a
+AL = team_2015[team_2015["League"]=="AL"]['HR'].astype('int64').mean()
+NL = team_2015[team_2015["League"]=="NL"]['HR'].astype('int64').mean()
+if AL>NL:
+    print("AL League higher for average number of homeruns be", AL)
+else:
+    print("NL League higher for average number of homeruns be", NL)
+#AL League higher for average number of homeruns be 182.5
+
+#b
+split_click = driver.find_element_by_id('st_hitting_hitting_splits')
+split_selcet=Select(split_click)
+split_selcet.select_by_visible_text('First Inning')
+
+first_inning_2015 = extract_stats_data(data_html)
+AL = first_inning_2015[first_inning_2015["League"]=="AL"]['HR'].astype('int64').mean()
+NL = first_inning_2015[first_inning_2015["League"]=="NL"]['HR'].astype('int64').mean()
+if AL>NL:
+    print("AL is higher. Its average number of homeruns is", AL)
+else:
+    print("NL is higher,Its average number of homerun is", NL)
